bfe8cbeb7ecc5818059d9937e442c91633122c2c
José Antonio dos Santos Barbosa <jose@josebarbosa.com.br>
2024-06-09 16:22:46 -0300

    Incluída tabela de razões e vencimentos. Ajustada MPU.csv

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ Aplicação desenvolvida por José Antonio dos Santos Barbosa
 contato: jose@josebarbosa.com.br
 '''
 
+import pandas as pd
+
 # TODO - carregar dados do dataframe /csv
+df_mpu = pd.read_csv('anexos/mpu.csv')
+df_mpu.info()
 
 # TODO - definir algumas regras de modelo (citar exemplos)
 # Exemplo: verificar qual o valor médio estimado para cálculo do impacto de AQ. É para considerar o valor total para todos ou existe um valor médio atual? Neste caso, qual seria o percentual aplicável
@@ -15,3 +19,8 @@ contato: jose@josebarbosa.com.br
 # TODO - calcular resultado
 
 # TODO - gerar planilha para interpretação pelos interessados
+
+# TODO - listar impacto orçamentário por
+#remuneração
+#contribuição patronal do PSS
+#auxílio-saúde
